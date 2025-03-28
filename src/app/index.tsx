a85eb145f4998c7ed78a45a3672c91189e17316d
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2025-03-28 15:29:35 -0300

    Criando pagina inicial

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,43 @@
-import { View, Text, StyleSheet } from 'react-native'
+import colors from '@/constants/colors';
+import { View, Text, StyleSheet, TextInput, Pressable } from 'react-native'
 
 export default function Login() {
   return (
     <View style={styles.container}>
-      <Text> Pagina Login</Text>
+      <View style={styles.header}>
+        
+        <Text style={styles.logoText}>
+          Dev <Text style={{color: colors.green}}>App</Text>
+        </Text>
+
+        <Text style={styles.slogan}> O futuro da Programação</Text>
+
+      </View>
+
+      <View style={styles.form}>
+        <View>
+          <Text> Email </Text>
+          <TextInput 
+            placeholder='Digite seu email...'
+            style={styles.input}
+          />
+        </View>
+
+        <View>
+          <Text> Senha </Text>
+          <TextInput 
+            placeholder='Digite sua senha...'
+            style={styles.input}
+            secureTextEntry
+          />
+        </View>
+
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}> Acessar</Text>
+        </Pressable>
+
+      </View>
+
     </View>
   )
 }
@@ -11,7 +45,58 @@ export default function Login() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        paddingTop:34, 
+        backgroundColor: colors.zinc
+    },
+    header: {
+      paddingLeft: 14,
+      paddingRight: 14
+    },
+    logoText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: colors.white,
+      marginBottom: 8,
+    },
+    slogan : {
+      fontSize: 34,
+      color: colors.white,
+      marginBottom: 34,
+    },
+    form : {
+      flex: 1,
+      backgroundColor: colors.white,
+      borderTopLeftRadius: 16,
+      borderTopRightRadius: 16,
+      paddingTop: 24,
+      paddingLeft: 14,
+      paddingRight: 14,
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: colors.gray,
+      borderRadius: 8,
+      marginBottom: 16,
+      paddingHorizontal: 8,
+      paddingTop: 14,
+      paddingBottom: 14,
+    },
+    label: {
+      color: colors.zinc,
+      marginBottom: 4,
+    },
+    button: {
+      backgroundColor: colors.green,
+      paddingTop: 14,
+      paddingBottom: 14,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+      borderRadius: 8,
+    },
+    buttonText: {
+      color: colors.zinc,
+      fontSize: 14,
+      fontWeight: 'bold'
     }
 });
